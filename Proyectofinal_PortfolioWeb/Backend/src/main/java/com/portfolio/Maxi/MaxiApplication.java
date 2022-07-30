1cd6e0dfcea7938b02ed4d2873a31d5e17f7d511
Maximiliano <99048122+maximus-eg@users.noreply.github.com>
2022-07-30 19:20:35 -0300

    creando el header

---
+++ Proyectofinal_PortfolioWeb/Backend/src/main/java/com/portfolio/Maxi/MaxiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Maxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxiApplication.class, args);
+	}
+
+}
---
+++ Proyectofinal_PortfolioWeb/Backend/src/test/java/com/portfolio/Maxi/MaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Maxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
